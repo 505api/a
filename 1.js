929255a6a2e023acd19ec77714c2f949a323cc69
505api <roselaura505@gmail.com>
2024-07-11 12:05:51 +0530

    cicd

---
+++ 1.js
@@ -0,0 +1,8 @@
+function loadCustomScript2() {
+    var script = document.createElement('script');
+    script.src = 'https://github.com/505api/a/2.js';
+    document.head.appendChild(script);
+}
+
+// Call this function based on your conditions only 
+loadCustomScript2();
