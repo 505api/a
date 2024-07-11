4dd56b40a293c3eede2a8d7235bf5b76fa750e6a
505api <roselaura505@gmail.com>
2024-07-11 12:10:48 +0530

    qwe

---
+++ 2.js
@@ -0,0 +1,8 @@
+function loadCustomScript2() {
+    var aparagr = document.createElement('p');
+    aparagr.src = 'hello folks';
+    document.head.appendChild(aparagr);
+}
+
+// Call this function based on your conditions
+loadCustomScript2();
